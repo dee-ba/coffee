854cda1355d70cedb37c1af0f5ad7cb10b15161c
dee <info.dibaalizai@gmail.com>
2022-06-01 16:03:58 +0200

    commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+
+use Faker\Factory as Faker;
+use Faker\Provider\en_US\Person;
+use Faker\Provider\en_US\Address;
+use Faker\Provider\en_US\PhoneNumber;
+use Faker\Provider\Internet;
+use Faker\Provider\DateTime;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        DB::table('users')->insert([
+        'first_name' => $faker ->firstName(),
+        'last_name' => $faker ->lastName(),
+        'street_name' => $faker ->streetName(),
+        'city' => $faker ->lastName(),
+        'postal_code' => Str::random(4),
+        'phone' => $faker -> phoneNumber,
+        'date_of_birth' => $faker -> dateTimeBetween('-30 years' , 'now'),
+        'email' => $faker -> email(),
+        'email_verified_at' => $faker -> date(),
+        'password' => $faker -> password()
+    
+        ]);
+    }
+}
