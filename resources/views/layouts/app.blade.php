8d3f4ebec3c7c4c7316519228d48622da7b8cafb
Diba <88105499+dee-ba@users.noreply.github.com>
2022-06-09 13:00:16 +0200

    user controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,10 +52,6 @@
 							</ul>
 						</div>	
 						
-						<div>
-							<a class="navbar-brand" href="{{ url('/users') }}">Users</a>
-						</div>
-						
 		
 						<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
 							<span class="navbar-toggler-icon"></span>
